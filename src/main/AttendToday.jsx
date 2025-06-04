fba58f03dc7fd6affe0926d5861765868cfe9dc1
hyunsupLee <akmei90@gmail.com>
2025-06-04 17:43:01 +0900

    독이 난무하는 오늘

--- src/main/AttendToday.jsx
+++ src/main/AttendToday.jsx
@@ -1,4 +1,4 @@
-import { supabase } from "../config/supabaseClient";
+import { supabase } from "../config/SupabaseClient";
 
 export const attendToday = async () => {
   const user = (await supabase.auth.getUser()).data.user;
