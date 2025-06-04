7293a24452589dc42dac89523ac50569c39bfccb
yjkdev21 <yjkdev21@gmail.com>
2025-06-04 17:26:47 +0900

    fix import name

--- src/service/QuizList.jsx
+++ src/service/QuizList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { supabase } from "../config/SupabaseClient";
-import "./quizList.css";
+import "./QuizList.css";
 import { Ellipsis } from "react-bootstrap/esm/PageItem";
 
 const categoryMap = {
