50c9a657b2848439a693eef28ad931ee37366e2c
hyunsupLee <akmei90@gmail.com>
2025-06-04 15:26:52 +0900

    attended check home add

--- src/main/Home.jsx
+++ src/main/Home.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import quizslide from "./mainSlide.svg";
 import { Container, Button } from "react-bootstrap";
 import "./Home.css";
+import { hasAttendedToday, attendToday } from "./AttendToday";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Home() {
   const message = "You Can Enjoy Unlimited Java quiz";
@@ -10,6 +12,30 @@ export default function Home() {
   const [welcomeMessage, setWelcomeMessage] = useState("");
   const [fadeOut, setFadeOut] = useState(false);
   const indexRef = useRef(0);
+  const auth = useAuth();
+
+  // 자동 출석 등록
+  const [attended, setAttended] = useState(false);
+  useEffect(() => {
+    const atcheck = async () => {
+      if (auth.session && !attended) {
+        let isAttend = await hasAttendedToday();
+        console.log("오늘 출석여부 : ", isAttend);
+        setAttended(isAttend);
+        if (!isAttend) attendClick();
+      }
+    };
+    atcheck();
+  }, []);
+
+  const attendClick = async () => {
+    if (attended) return;
+    console.log("attendClick..", attended);
+    await attendToday();
+    setAttended(true);
+  };
+
+  //
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -46,6 +72,16 @@ export default function Home() {
           {welcomeMessage}
         </div>
       )}
+
+      {/* <button
+        onClick={attendClick}
+        disabled={attended}
+        className={`px-4 py-2 rounded ${
+          attended ? "bg-primary" : "bg-primary hover:bg-green-600"
+        } text-white`}
+      >
+        {attended ? "오늘 출석 완료" : "출석하기"}
+      </button> */}
 
       <h4 className="fw-bold mt-5">자바냥 에서는</h4>
       <h2 className="fw-bold display-5 mb-3">5분마다 인생이 바뀐다</h2>
